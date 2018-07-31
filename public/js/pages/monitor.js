ec918d161b4bde96723f9de9b3222e4c5058bc9d
cymon1997 <cybermonster82@yahoo.com>
2018-07-31 21:43:26 +0700

    added working graph

--- public/js/pages/monitor.js
+++ public/js/pages/monitor.js
@@ -1,3 +1,60 @@
 $(document).ready(function() {
     console.log('monitor loaded');
+
+    var heartbeat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var temperature = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var tempChart = new Chartist.Line('#temperature', {
+        // labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        series: [
+            [temperature.slice(temperature.length-30, temperature.length)],
+        ]
+    }, {
+        high: 50,
+        low: 0,
+        fullWidth: true,
+        chartPadding: {
+            right: 40
+        }
+    });
+
+    var beatChart = new Chartist.Line('#heartbeat', {
+        // labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        series: [
+            [heartbeat.slice(heartbeat.length-30, heartbeat.length)],
+        ]
+    }, {
+        high: 200,
+        low: 0,
+        fullWidth: true,
+        chartPadding: {
+            right: 40
+        }
+    });
+
+    function render() {
+        var newBeat = getRandomInt(1, 200);
+        document.getElementById('heartbeat-current').innerHTML = newBeat;
+        var newTemp = getRandomInt(1, 50);
+        document.getElementById('temperature-current').innerHTML = newTemp;
+        heartbeat.push(newBeat);
+        temperature.push(newTemp);
+        // var newSeries = {series: [[increment, increment+1, increment-2, increment+3, increment]]};
+        var beatSeries = {
+            series: [heartbeat.slice(heartbeat.length-30, heartbeat.length)]
+        };
+        var tempSeries = {
+            series: [temperature.slice(temperature.length-30, temperature.length)]
+        };
+        beatChart.update(beatSeries);
+        tempChart.update(tempSeries);
+        console.log('updated');
+        setTimeout(render, 500);
+    }
+
+    render();
 });
--- resources/views/pages/monitor.blade.php
+++ resources/views/pages/monitor.blade.php
@@ -7,9 +7,26 @@
 @endsection
 
 @section('content')
-<div class="">
-    
-</div>
+<div class="container" style="margin-top: 35px;">
+    <h3 id="temperature-title">Temperature</h3>
+    <div class="grid-x">
+        <div class="cell small-10">
+            <div id="temperature" class="ct-chart"></div>
+        </div>
+        <div class="cell small-2">
+            <h1 id="temperature-current">0</h1>
+        </div>
+    </div>
+    <h3 id="heartbeat-title">Heartbeat</h3>
+    <div class="grid-x">
+        <div class="cell small-10">
+            <div id="heartbeat" class="ct-chart"></div>
+        </div>
+        <div class="cell small-2">
+            <h1 id="heartbeat-current">0</h1>
+        </div>
+    </div>
+    </div>
 @endsection
 
 @section('extra-js')
